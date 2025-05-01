df816908ecb76008cb8f2c37881cb6718392f6aa
Zaryab <mzaryabuddin@gmail.com>
2025-05-01 14:21:42 +0500

    mongodb added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// MONGO
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Loads .env variables
+    MongooseModule.forRoot(process.env.MONGODB_URI), // Connects to MongoDB
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
